ee1a2561f2fd964d833b982154fd90eb803bb18c
psenY <81437183+psenY@users.noreply.github.com>
2021-09-27 00:58:53 +0800

    修正物品分类标签
    Co-Authored-By: ThrRip <54434330+ThrRip@users.noreply.github.com>

--- src/main/java/utils/ItemTags.java
+++ src/main/java/utils/ItemTags.java
@@ -7,7 +7,7 @@ package utils;
 public class ItemTags {
 
     public static final String CRAFTING_ITEM = "&8原材料";
-    public static final String MULTIBLOCK = "&8多方块";
+    public static final String MULTIBLOCK = "&8多方块结构";
     public static final String TOOL = "&8工具";
     public static final String MAGICAL_ITEM = "&8魔法物品";
 
